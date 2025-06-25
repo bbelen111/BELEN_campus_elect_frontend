cd74c20c5effb45cc79cf61fcea695f0d9d28d40
Brendon Justine Alvero Belen <bjabelen@addunet.ph>
2025-06-25 13:52:11 +0800

    fix: enhance Home component header styling and add subtitle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Brendon Justine Belen</h1>
+        <h1 className="text-2xl font-bold">
+          Brendon Justine Belen
+        </h1>
+        <span className="text-sm font-normal">CAMPUS-ELECT-FRONTEND</span>
         <StudentsList/>
         <Image
           className="dark:invert"
